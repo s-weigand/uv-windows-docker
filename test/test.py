329ae22e141699749bf2b6e173f48bb01ed83ccb
s-weigand <s.weigand.phy@gmail.com>
2024-12-07 21:42:34 +0100

    🧪 Add test to the CI pipeline

---
+++ test/test.py
@@ -0,0 +1,8 @@
+from git_install_test_distribution import __version__
+
+def main():
+    print("Was able to het version of git_install_test_distribution:",__version__)
+
+
+if __name__ == "__main__":
+    main()
