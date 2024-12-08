90b84f51acde347294f6c419ce96e04fd0c98fa6
Sebastian Weigand <s.weigand.phy@gmail.com>
2024-12-08 15:35:47 +0100

    ✨ Add git and git-lfs to images (#8)
    This change adds [git for windows](https://github.com/git-for-windows/git) and [git LFS](https://github.com/git-lfs/git-lfs) to the images.
    
    Main reason is to support `uv` installation of packages that have direct dependencies on packages from `git` like in the added test example:
    ```toml
    [tool.uv.sources]
    git_install_test_distribution = { git = "https://github.com/s-weigand/git-install-test-distribution" }
    ```

---
+++ test/test.py
@@ -0,0 +1,8 @@
+from git_install_test_distribution import __version__
+
+def main():
+    print("Was able to get version of git_install_test_distribution:",__version__)
+
+
+if __name__ == "__main__":
+    main()
