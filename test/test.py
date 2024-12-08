9861bd34f32279e67889f7bfb66872548fb9d309
s-weigand <s.weigand.phy@gmail.com>
2024-12-08 15:13:59 +0100

    🧹 Fix typo in python print

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 from git_install_test_distribution import __version__
 
 def main():
-    print("Was able to het version of git_install_test_distribution:",__version__)
+    print("Was able to get version of git_install_test_distribution:",__version__)
 
 
 if __name__ == "__main__":
